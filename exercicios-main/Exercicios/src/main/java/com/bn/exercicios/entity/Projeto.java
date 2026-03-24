b190847444c3f7399758c25d2cf980ffdfecf01a
Gabriel Lopes <gabrielri139@gmail.com>
2026-03-24 10:27:36 -0300

    Criação da Entidade Projeto

---
+++ exercicios-main/Exercicios/src/main/java/com/bn/exercicios/entity/Projeto.java
@@ -0,0 +1,54 @@
+package com.bn.exercicios.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "TBL_PROJETO")
+public class Projeto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+
+    public Projeto(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+}
