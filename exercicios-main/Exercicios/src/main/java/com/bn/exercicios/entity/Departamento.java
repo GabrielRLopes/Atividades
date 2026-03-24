7cb13e856874548eeac881ce5b1a2722d76a286c
Gabriel Lopes <gabrielri139@gmail.com>
2026-03-24 10:13:28 -0300

    Criaçao da Entidade Departamento

---
+++ exercicios-main/Exercicios/src/main/java/com/bn/exercicios/entity/Departamento.java
@@ -0,0 +1,42 @@
+package com.bn.exercicios.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TBL_DEPARTAMENTO")
+public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String localizacao;
+
+    public Departamento(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+}
