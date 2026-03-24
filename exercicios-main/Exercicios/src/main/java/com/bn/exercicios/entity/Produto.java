374bc70abca6a858578f6a42015d041e1dea3ca0
Gabriel Lopes <gabrielri139@gmail.com>
2026-03-24 07:56:10 -0300

    Criação da entidade Produto

---
+++ exercicios-main/Exercicios/src/main/java/com/bn/exercicios/entity/Produto.java
@@ -0,0 +1,54 @@
+package com.bn.exercicios.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "TBL_LIVRO")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private BigDecimal preco;
+    private Integer estoque;
+
+    public Produto(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+}
