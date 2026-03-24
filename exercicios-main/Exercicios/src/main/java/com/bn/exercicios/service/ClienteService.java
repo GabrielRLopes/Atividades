bc2b82f09b3b80ce6665e7fe71fbfcc38920e5e0
Gabriel Lopes <gabrielri139@gmail.com>
2026-03-24 09:42:39 -0300

    Criação do ClienteService

---
+++ exercicios-main/Exercicios/src/main/java/com/bn/exercicios/service/ClienteService.java
@@ -0,0 +1,30 @@
+package com.bn.exercicios.service;
+
+import com.bn.exercicios.entity.Cliente;
+import com.bn.exercicios.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente criarAutor(Cliente cliente){
+        return  clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> findAll(){
+        return  clienteRepository.findAll();
+    }
+
+    public void deletarCliente(Long id){
+        clienteRepository.deleteById(id);
+    }
+
+
+
+}
