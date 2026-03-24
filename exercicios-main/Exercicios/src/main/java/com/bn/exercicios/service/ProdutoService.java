845520d87d7ec2393ca30c890e32996bce3f3c67
Gabriel Lopes <gabrielri139@gmail.com>
2026-03-24 08:05:52 -0300

    Criação do ProdutoService

---
+++ exercicios-main/Exercicios/src/main/java/com/bn/exercicios/service/ProdutoService.java
@@ -0,0 +1,30 @@
+package com.bn.exercicios.service;
+
+import com.bn.exercicios.entity.Produto;
+import com.bn.exercicios.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto criarProduto(Produto produto){
+        return  produtoRepository.save(produto);
+    }
+
+    public List<Produto> findAll(){
+        return  produtoRepository.findAll();
+    }
+
+    public void deletarProduto(Long id){
+        produtoRepository.deleteById(id);
+    }
+
+
+
+}
