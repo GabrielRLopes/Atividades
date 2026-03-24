925750e56dcb5394a4d7fd2ae4499abe653acc89
Gabriel Lopes <gabrielri139@gmail.com>
2026-03-24 09:58:23 -0300

    Crialç~ão do FornecedorService

--- exercicios-main/Exercicios/src/main/java/com/bn/exercicios/service/FornecedorService.java
+++ exercicios-main/Exercicios/src/main/java/com/bn/exercicios/service/FornecedorService.java
@@ -1,4 +1,30 @@
 package com.bn.exercicios.service;
 
+import com.bn.exercicios.entity.Fornecedor;
+import com.bn.exercicios.repository.FornecedorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FornecedorService {
+
+    @Autowired
+    private FornecedorRepository fornecedorRepository;
+
+    public Fornecedor criarFornecedor(Fornecedor fornecedor){
+        return  fornecedorRepository.save(fornecedor);
+    }
+
+    public List<Fornecedor> findAll(){
+        return  fornecedorRepository.findAll();
+    }
+
+    public void deletarFornecedor(Long id){
+        fornecedorRepository.deleteById(id);
+    }
+
+
+
 }
